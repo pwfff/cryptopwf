4804aa3d979647ef218257736329b14c94319623
pwf <aaron.lisman@gmail.com>
2014-12-03 20:18:42 -0600

    added tests for challenge verification

--- src/main/scala/com/cryptopwf/Challenge1.scala
+++ src/main/scala/com/cryptopwf/Challenge1.scala
@@ -5,13 +5,9 @@ import sun.misc.BASE64Encoder
 
 object Challenge1 {
   val encoder = new BASE64Encoder()
-  def main(args: Array[String]): Unit = {
-    val hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 
-    println("Input: " + hex)
-
+  def hex2base64(hex: String): String = {
     val bytes = HexBytesUtil.hex2bytes(hex)
-
-    println("Output: " + encoder.encode(bytes))
+    encoder.encode(bytes)
   }
 }
---
+++ src/test/scala/com/cryptopwf/Challenge1Spec.scala
@@ -0,0 +1,12 @@
+import com.cryptopwf.Challenge1
+import org.scalatest._
+
+class Challenge1Spec extends FlatSpec with Matchers {
+
+  "Challenge1" should "convert hex strings to base64 encoded bytes" in {
+    val hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+    val base64 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+
+    Challenge1.hex2base64(hex) should be (base64)
+  }
+}
