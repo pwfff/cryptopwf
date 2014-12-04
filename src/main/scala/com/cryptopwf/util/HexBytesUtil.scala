f1d066f8b8a94f3a4b0e38e6727fa9a4e3f80086
pwf <aaron.lisman@gmail.com>
2014-12-04 13:00:59 -0600

    changing bytes2hex, adding attribution

--- src/main/scala/com/cryptopwf/util/HexBytesUtil.scala
+++ src/main/scala/com/cryptopwf/util/HexBytesUtil.scala
@@ -1,17 +1,14 @@
+/*
+ * Ripped from https://gist.github.com/tmyymmt/3727124 and modified
+ */
 package com.cryptopwf.util
 
 object HexBytesUtil {
- 
   def hex2bytes(hex: String): Array[Byte] = {
     hex.replaceAll("[^0-9A-Fa-f]", "").sliding(2, 2).toArray.map(Integer.parseInt(_, 16).toByte)
   }
- 
-  def bytes2hex(bytes: Array[Byte], sep: Option[String] = None): String = {
-    sep match {
-      case None => bytes.map("%02x".format(_)).mkString
-      case _ => bytes.map("%02x".format(_)).mkString(sep.get)
-    }
-    // bytes.foreach(println)
+
+  def bytes2hex(bytes: Array[Byte]): String = {
+    bytes.map("%02x".format(_)).mkString
   }
- 
 }
