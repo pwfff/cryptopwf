2d1430ae0b65e73db0190c736458788e8a8b1ee6
pwf <aaron.lisman@gmail.com>
2014-12-04 13:01:19 -0600

    challenge 2 done

---
+++ src/main/scala/com/cryptopwf/Challenge2.scala
@@ -0,0 +1,14 @@
+package com.cryptopwf
+
+import com.cryptopwf.util.HexBytesUtil
+
+object Challenge2 {
+  def xorhex(hex1: String, hex2: String): String = {
+    val bytes1 = HexBytesUtil.hex2bytes(hex1)
+    val bytes2 = HexBytesUtil.hex2bytes(hex2)
+
+    val xored: Array[Byte] = (bytes1, bytes2).zipped map {(a, b) => (a ^ b).toByte}
+
+    HexBytesUtil.bytes2hex(xored)
+  }
+}
---
+++ src/test/scala/com/cryptopwf/Challenge2Spec.scala
@@ -0,0 +1,13 @@
+import com.cryptopwf.Challenge2
+import org.scalatest._
+
+class Challenge2Spec extends FlatSpec with Matchers {
+  "Challenge2" should "xor two hex strings" in {
+    val hex1 = "1c0111001f010100061a024b53535009181c"
+    val hex2 = "686974207468652062756c6c277320657965"
+
+    val result = "746865206b696420646f6e277420706c6179"
+
+    Challenge2.xorhex(hex1, hex2) should be (result)
+  }
+}
