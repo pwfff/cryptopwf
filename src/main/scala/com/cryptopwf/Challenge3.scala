3a8b3cff66ef2281b6bf38f0f38e8312f3abd6c7
pwf <aaron.lisman@gmail.com>
2014-12-04 15:13:33 -0600

    some refactoring

--- src/main/scala/com/cryptopwf/Challenge3.scala
+++ src/main/scala/com/cryptopwf/Challenge3.scala
@@ -3,23 +3,29 @@ package com.cryptopwf
 import com.cryptopwf.util.HexBytesUtil
 
 object Challenge3 {
+  implicit class CharProperties(val ch: Char) extends AnyVal {
+    def isASCIILetter: Boolean = (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')
+  }
+
   def decrypt(hex: String): String = {
     val bytes = HexBytesUtil.hex2bytes(hex)
 
-    val allBytes = (0 to 255).view.map(_.toByte).toArray
+    val allBytes = (0 to 255).view.map(_.toByte)
 
     val scores = allBytes map(b => b -> score(bytes, b)) toMap
 
     val best = scores.maxBy(_._2)._1
 
-    val unencrypted = bytes.map(_ ^ best)
+    val decrypted = bytes.map(_ ^ best)
 
-    unencrypted.map(_.toChar).mkString
+    decrypted.map(_.toChar).mkString
   }
 
   def score(bytes: Array[Byte], candidate: Byte): Int = {
-    bytes.foldLeft(0) {
-      case (total, byte) if ((byte ^ candidate) >= 'a' && (byte ^ candidate) <= 'z') || ((byte ^ candidate) >= 'A' && (byte ^ candidate) <= 'Z') => total + 1
+    val decrypted = bytes.map(_ ^ candidate)
+
+    decrypted.foldLeft(0) {
+      case (total, byte) if byte.toChar.isASCIILetter => total + 1
       case (total, _) => total
     }
   }
