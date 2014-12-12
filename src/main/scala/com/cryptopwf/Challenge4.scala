25b054f8d4713618065272f864403dacec9dcfe1
pwf <aaron.lisman@gmail.com>
2014-12-12 16:42:04 -0600

    tiny refactoring

--- src/main/scala/com/cryptopwf/Challenge4.scala
+++ src/main/scala/com/cryptopwf/Challenge4.scala
@@ -5,7 +5,7 @@ import com.cryptopwf.util.ASCII._
 
 object Challenge4 {
   def decrypt(hexes: List[String]): String = {
-    val decrypted = hexes map(hex => Challenge3.decrypt(hex))
+    val decrypted = hexes map Challenge3.decrypt
 
     val scores = decrypted map(s => s -> s.count(_.isASCIILetter))
 
