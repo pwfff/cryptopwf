ce7e41d383407f63d1f986e6e0a21290f9dd3198
pwf <aaron.lisman@gmail.com>
2014-12-16 13:36:50 -0600

    removing ASCII module

--- src/main/scala/com/cryptopwf/Challenge5.scala
+++ src/main/scala/com/cryptopwf/Challenge5.scala
@@ -1,7 +1,6 @@
 package com.cryptopwf
 
 import com.cryptopwf.util.RepeatingKeyXOR
-import com.cryptopwf.util.ASCII._
 
 object Challenge5 {
   def encrypt(plaintext: String, key: String): String = {
