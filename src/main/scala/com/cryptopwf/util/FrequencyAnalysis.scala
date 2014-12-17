855408c46f51b196af42179e25cc277b359b349d
pwf <aaron.lisman@gmail.com>
2014-12-17 14:52:16 -0600

    b0lt's optimization

--- src/main/scala/com/cryptopwf/util/FrequencyAnalysis.scala
+++ src/main/scala/com/cryptopwf/util/FrequencyAnalysis.scala
@@ -1,30 +1,39 @@
+/*
+ * stolen from b0lt on IRC because he typed it before I did...
+ */
 package com.cryptopwf.util
 
 object FrequencyAnalysis {
-  val englishLetterFreq: Map[Char, Double] = Map(
-    'E' -> .1270, 'T' -> .0906, 'A' -> .0817, 'O' -> .0751, 'I' -> .0697,
-    'N' -> .0675, 'S' -> .0633, 'H' -> .0609, 'R' -> .0599, 'D' -> .0425,
-    'L' -> .0403, 'C' -> .0278, 'U' -> .0276, 'M' -> .0241, 'W' -> .0236,
-    'F' -> .0223, 'G' -> .0202, 'Y' -> .0197, 'P' -> .0193, 'B' -> .0129,
-    'V' -> .0098, 'K' -> .0077, 'J' -> .0015, 'X' -> .0015, 'Q' -> .0010,
-    'Z' -> .0007, ' ' -> .1300
-  )
+  val englishLetterFreq: Array[Double] = Array(0.0817, 0.0129, 0.0278, 0.0425, 0.127, 0.0223, 0.0202, 0.0609, 0.0697, 0.0015, 0.0077, 0.0403, 0.0241, 0.0675, 0.0751, 0.0193, 0.001, 0.0599, 0.0633, 0.0906, 0.0276, 0.0098, 0.0236, 0.0015, 0.0197, 0.0007,
+0.13, 0.0)
 
-  def getLetterFrequency(text: String): Map[Char, Double] = {
-    /* stolen from http://stackoverflow.com/questions/12105130/generating-a-frequency-map-for-a-string-in-scala */
-    text.groupBy(_.toUpper).map{ t => (t._1, t._2.length.toDouble / text.length) }
+  def charIndex(char: Char): Int = {
+    if (char == ' ') {
+      26
+    } else {
+      val c = char.toUpper - 'A'
+      if (c < 0 || c > 25) {
+        27
+      } else {
+        c
+      }
+    }
   }
 
   def englishScore(text: String): Double = {
-    val letterFreq = getLetterFrequency(text)
+    val stringCount = new Array[Int](28)
+    text.foreach { c =>
+      stringCount(charIndex(c)) += 1
+    }
 
-    text.map {
-      case c => (englishLetterFreq.get(c.toUpper), letterFreq.get(c.toUpper))
-    }.map {
-      case (Some(a), Some(b)) => math.abs(a - b)
-      case (Some(a), None) => a
-      case _ => 1
-    }.sum
+    val stringFreq = stringCount map { _.asInstanceOf[Double] / text.length() }
+
+    var counter = 0.0;
+    for (i <- 0 to 27) {
+      counter += Math.abs(englishLetterFreq(i) - stringFreq(i))
+    }
+
+    counter
   }
 
   def mostEnglishText(candidates: collection.GenSeq[String]): String = {
