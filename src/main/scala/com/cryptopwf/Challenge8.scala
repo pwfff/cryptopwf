20b90cf065b88e44f163b44f85621ffbea229411
pwf <aaron.lisman@gmail.com>
2014-12-19 14:01:00 -0600

    challenge 8 done

---
+++ src/main/scala/com/cryptopwf/Challenge8.scala
@@ -0,0 +1,11 @@
+package com.cryptopwf
+
+import com.cryptopwf.util.HexBytesUtil
+
+object Challenge8 {
+  def detectECB(hexes: IndexedSeq[String]): String = {
+    val bytes = hexes map HexBytesUtil.hex2bytes
+
+    HexBytesUtil.bytes2hex(bytes.minBy(_.grouped(16).toIndexedSeq.distinct.length))
+  }
+}
