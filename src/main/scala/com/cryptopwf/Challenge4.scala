e7bd6418b99e038c80fde16d5ce594f1c61c19e2
pwf <aaron.lisman@gmail.com>
2014-12-19 13:46:05 -0600

    refactoring some test stuff

--- src/main/scala/com/cryptopwf/Challenge4.scala
+++ src/main/scala/com/cryptopwf/Challenge4.scala
@@ -4,7 +4,7 @@ import com.cryptopwf.util.HexBytesUtil
 import com.cryptopwf.util.FrequencyAnalysis
 
 object Challenge4 {
-  def decrypt(hexes: List[String]): String = {
+  def decrypt(hexes: IndexedSeq[String]): String = {
     val candidates = hexes.par.map(Challenge3.decrypt)
 
     FrequencyAnalysis.mostEnglishText(candidates)
